6bcea5a89afd1307e146da9468b8fa0b57acc21e
leandro <leandro.simoes@outlook.com>
2016-10-12 15:23:25 -0300

    First commit;

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # br-docs
-Brazilian documents validations.
+
+Validation for the most commom personal Brazilian documents (ID's). 
+
+### How to?
+
+#### CPF
+``` html
+    <!-- Add this in your page -->
+    <script src="cpf.js"></script>
+```
+
+``` javascript
+    //224.407.595-89 is a valid CPF sample
+    CPF.validar('224.407.595-89'); //true or false
+```
+
+#### CNPJ
+``` html
+    <!-- Add this in your page -->
+    <script src="cnpj.js"></script>
+```
+
+``` javascript
+    //29.349.923/0001-35 is a valid CNPJ sample
+    CNPJ.validar('29.349.923/0001-35'); //true or false
+```
+
+#### IE
+``` html
+    <!-- Add this in your page -->
+    <script src="ie.js"></script>
+```
+
+``` javascript
+    //358.829.924.310 is a valid IE sample
+    //'sp' is the state of Brazil that you want to validate, in this
+    //case 'sp' (São Paulo)
+    IE.validar('358.829.924.310', 'sp'); //true or false
+    
+    //Return true if in some of states of Brazil this IE is valid
+    //358.829.924.310 is a valid IE sample
+    IE.temEstadoValido('358.829.924.310'); //true or false
+
+    //Return the state initials that this IE is valid
+    //358.829.924.310 is a valid IE sample
+    IE.obterEstadoValido('358.829.924.310') //in this case return 'sp' (São Paulo)
+```
+
+#### PIS
+``` html
+    <!-- Add this in your page -->
+    <script src="pis.js"></script>
+```
+
+``` javascript
+    //120.7507.914-7 is a valid PIS sample
+    PIS.validar('120.7507.914-7'); //true or false
+```
---
+++ src/cnpj.js
@@ -0,0 +1,58 @@
+﻿; (function (commonjs) {
+    'use strict';
+
+    var comuns = [
+      "00000000000000",
+      "11111111111111",
+      "22222222222222",
+      "33333333333333",
+      "44444444444444",
+      "55555555555555",
+      "66666666666666",
+      "77777777777777",
+      "88888888888888",
+      "99999999999999"
+    ];
+
+    var digitoVerificador = function (numeros) {
+        var index = 2,
+            reverse = numeros.split("").reduce(function (atual, prox) {
+                return [parseInt(prox, 10)].concat(atual);
+            }, []),
+            total = reverse.reduce(function (atual, prox) {
+                atual += prox * index;
+                index = (index === 9 ? 2 : index + 1);
+                return atual;
+            }, 0);
+
+        var mod = total % 11;
+        return (mod < 2 ? 0 : 11 - mod);
+    };
+
+    var CNPJ = {};
+
+    CNPJ.validar = function (cnpj) {
+        cnpj = (cnpj || "").toString().replace(/[^\d]/g, "");
+
+        // CNPJ must be defined
+        if (!cnpj) { return false; }
+
+        // CNPJ must have 14 chars
+        if (cnpj.length !== 14) { return false; }
+
+        // CNPJ can't be comunsed
+        if (comuns.indexOf(cnpj) >= 0) { return false; }
+
+        var numeros = cnpj.substr(0, 12);
+        numeros += digitoVerificador(numeros);
+        numeros += digitoVerificador(numeros);
+
+        return numeros.substr(-2) === cnpj.substr(-2);
+    };
+
+    if (commonjs) {
+        module.exports = CNPJ;
+    } else {
+        window.CNPJ = CNPJ;
+    }
+})(typeof (exports) !== "undefined");
